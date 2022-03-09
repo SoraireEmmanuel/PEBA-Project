9d740fa91e7e65238ba712fa4835c44a7e2b47a9
msoraire <msoraire@dev.azure.com>
2022-03-09 00:25:27 -0300

    ProtocolPagePart11-ValidationLectura

--- src/app/pages/newProtocol/reading/reading.component.ts
+++ src/app/pages/newProtocol/reading/reading.component.ts
@@ -21,17 +21,108 @@ DropdownOptions:QuestionStep5=new QuestionStep5();
 LecturaCuantitativa:Lectura_CuantitativaDTO=new Lectura_CuantitativaDTO();
 LecturaCualitativa:Lectura_CualitativaDTO=new Lectura_CualitativaDTO();
 CopyLecturaCualitativa:Lectura_CualitativaDTO=new Lectura_CualitativaDTO();
+popupsave:boolean;
   constructor(public _ModalService:NgbModal, config: NgbModalConfig) {
     config.backdrop = 'static';
     config.keyboard = false;
    }
-
   ngOnInit(): void {
     this.LecturaCualitativa=this.lecturacualitativa;
     this.LecturaCuantitativa=this.lecturacuantitativa;
-    this.CopyLecturaCualitativa=this.lecturacualitativa;
+    //this.CopyLecturaCualitativa=this.lecturacualitativa;
+    this.copycharge();
+    this.popUpValidation();
   }
+copycharge(){
+  this.CopyLecturaCualitativa.CDCEAlteracionDelOrden=this.lecturacualitativa.CDCEAlteracionDelOrden;
+  this.CopyLecturaCualitativa.CDCEOmisionDeParte=this.lecturacualitativa.CDCEOmisionDeParte;
+  this.CopyLecturaCualitativa.CDCESustitucionDeParte=this.lecturacualitativa.CDCESustitucionDeParte;
+  this.CopyLecturaCualitativa.ComprensionDeComandoEscrito=this.lecturacualitativa.ComprensionDeComandoEscrito;
+  this.CopyLecturaCualitativa.EPEDErroresFormales=this.lecturacualitativa.EPEDErroresFormales;
+  this.CopyLecturaCualitativa.EPEDErroresNoRelacionados=this.lecturacualitativa.EPEDErroresNoRelacionados;
+  this.CopyLecturaCualitativa.EPEDErroresSemanticos=this.lecturacualitativa.EPEDErroresSemanticos;
+  this.CopyLecturaCualitativa.EmparejamientoPalabraEscritaDibujo=this.lecturacualitativa.EmparejamientoPalabraEscritaDibujo;
+  this.CopyLecturaCualitativa.LNPLexicalizacionDeNoPalabras=this.lecturacualitativa.LNPLexicalizacionDeNoPalabras;
+  this.CopyLecturaCualitativa.LNPNoPalabrasErroneas=this.lecturacualitativa.LNPNoPalabrasErroneas;
+  this.CopyLecturaCualitativa.LPParalexiasFormales=this.lecturacualitativa.LPParalexiasFormales;
+  this.CopyLecturaCualitativa.LPParalexiasMorfologicas=this.lecturacualitativa.LPParalexiasMorfologicas;
+  this.CopyLecturaCualitativa.LPParalexiasSemanticas=this.lecturacualitativa.LPParalexiasSemanticas;
+  this.CopyLecturaCualitativa.LPSustitucionPorNoPalabras=this.lecturacualitativa.LPSustitucionPorNoPalabras;
+  this.CopyLecturaCualitativa.LecturaDeNoPalabras=this.lecturacualitativa.LecturaDeNoPalabras;
+  this.CopyLecturaCualitativa.LecturaDePalabras=this.lecturacualitativa.LecturaDePalabras;
+}
+clear(){
+  if (this.LecturaCualitativa.ComprensionDeComandoEscrito == 0){
+    this.LecturaCualitativa.CDCEOmisionDeParte=null;
+    this.LecturaCualitativa.CDCEAlteracionDelOrden=null;
+    this.LecturaCualitativa.CDCESustitucionDeParte=null
+  }
+  if(this.LecturaCualitativa.LecturaDeNoPalabras == 0){
+    this.LecturaCualitativa.LNPLexicalizacionDeNoPalabras=null;
+    this.LecturaCualitativa.LNPNoPalabrasErroneas=null;
+  }
+  if(this.LecturaCualitativa.LecturaDePalabras == 0){
+    this.LecturaCualitativa.LPParalexiasFormales=null;
+    this.LecturaCualitativa.LPParalexiasMorfologicas=null;
+    this.LecturaCualitativa.LPParalexiasSemanticas=null;
+    this.LecturaCualitativa.LPSustitucionPorNoPalabras=null;
+  }
+  if(this.LecturaCualitativa.EmparejamientoPalabraEscritaDibujo==0){
+    this.LecturaCualitativa.EPEDErroresFormales=null;
+    this.LecturaCualitativa.EPEDErroresNoRelacionados=null;
+    this.LecturaCualitativa.EPEDErroresSemanticos=null;
+  }
+}
+popUpValidation(){
+  var CDCE:boolean;
+  var EPED:boolean;
+  var LP:boolean;
+  var LNP:boolean;
+  if(this.LecturaCualitativa.ComprensionDeComandoEscrito == null && this.LecturaCualitativa.EmparejamientoPalabraEscritaDibujo==null&&
+    this.LecturaCualitativa.LecturaDeNoPalabras == null && this.LecturaCualitativa.LecturaDePalabras == null){
+    this.popupsave = true;
+  }
+  if (this.LecturaCualitativa.ComprensionDeComandoEscrito == 1 &&
+    (this.LecturaCualitativa.CDCEAlteracionDelOrden == null || this.LecturaCualitativa.CDCEAlteracionDelOrden == false)&&
+    (this.LecturaCualitativa.CDCEOmisionDeParte == null || this.LecturaCualitativa.CDCEOmisionDeParte == false)&&
+    (this.LecturaCualitativa.CDCESustitucionDeParte == null || this.LecturaCualitativa.CDCESustitucionDeParte == false)){
+    CDCE = false
+  } else {
+    CDCE = true
+  }
+  if(this.LecturaCualitativa.EmparejamientoPalabraEscritaDibujo == 1 &&
+    (this.LecturaCualitativa.EPEDErroresFormales == null || this.LecturaCualitativa.EPEDErroresFormales == false)&&
+    (this.LecturaCualitativa.EPEDErroresNoRelacionados == null || this.LecturaCualitativa.EPEDErroresNoRelacionados == false)&&
+    (this.LecturaCualitativa.EPEDErroresSemanticos == null || this.LecturaCualitativa.EPEDErroresSemanticos == false)){
+      EPED = false
+    }else{
+      EPED = true
+    }
+    if(this.LecturaCualitativa.LecturaDeNoPalabras == 1 &&
+      (this.LecturaCualitativa.LNPLexicalizacionDeNoPalabras == null || this.LecturaCualitativa.LNPLexicalizacionDeNoPalabras == false)&&
+      (this.LecturaCualitativa.LNPNoPalabrasErroneas == null || this.LecturaCualitativa.LNPNoPalabrasErroneas == false)){
+        LNP = false
+    }else{
+      LNP = true
+    }
 
+    if(this.LecturaCualitativa.LecturaDePalabras==1 &&
+      (this.LecturaCualitativa.LPParalexiasFormales == null || this.LecturaCualitativa.LPParalexiasFormales == false)&&
+      (this.LecturaCualitativa.LPParalexiasMorfologicas == null || this.LecturaCualitativa.LPParalexiasMorfologicas == false)&&
+      (this.LecturaCualitativa.LPParalexiasSemanticas == null || this.LecturaCualitativa.LPParalexiasSemanticas == false)&&
+      (this.LecturaCualitativa.LPSustitucionPorNoPalabras == null || this.LecturaCualitativa.LPSustitucionPorNoPalabras == false)){
+      LP = false
+    }else{
+      LP = true
+    }
+    if(LP && LNP && EPED && CDCE){
+      this.popupsave=true
+    }else{
+      this.popupsave=false
+    }
+
+
+  }
   open(register){
     const modalRef = this._ModalService.open(register, { size: 'xl' })
   }
@@ -92,11 +183,35 @@ CopyLecturaCualitativa:Lectura_CualitativaDTO=new Lectura_CualitativaDTO();
     this.LecturaCualitativa.LecturaDePalabras=this.CopyLecturaCualitativa.LecturaDePalabras;
 
   }
-
+cuantitativoFieldValidation():boolean{
+  if ((this.LecturaCuantitativa.Arco == null || this.LecturaCuantitativa.Bleja == null ||
+    this.LecturaCuantitativa.Casa == null || this.LecturaCuantitativa.Cho == null ||
+    this.LecturaCuantitativa.Comunidad == null || this.LecturaCuantitativa.Estatua == null ||
+    this.LecturaCuantitativa.Grito == null || this.LecturaCuantitativa.Mono == null ||
+    this.LecturaCuantitativa.Ne == null || this.LecturaCuantitativa.SaqueLaMano == null ||
+    this.LecturaCuantitativa.Tudipro == null) &&
+    this.LecturaCuantitativa.WasSuspended == false
+  ) {
+    return false;
+  }
+  if (this.LecturaCuantitativa.WasSuspended == true) {
+    return true;
+  }
+  if (this.LecturaCuantitativa.Arco != null || this.LecturaCuantitativa.Bleja != null ||
+    this.LecturaCuantitativa.Casa != null || this.LecturaCuantitativa.Cho != null ||
+    this.LecturaCuantitativa.Comunidad != null || this.LecturaCuantitativa.Estatua != null ||
+    this.LecturaCuantitativa.Grito != null || this.LecturaCuantitativa.Mono != null ||
+    this.LecturaCuantitativa.Ne != null || this.LecturaCuantitativa.SaqueLaMano != null ||
+    this.LecturaCuantitativa.Tudipro != null) {
+    return true
+ }
+}
   next() {
-    this.event.emit(1)
-    this.emitCualitativa.emit(this.LecturaCualitativa);
-    this.emitCuantitativa.emit(this.LecturaCuantitativa);
+    if(this.cuantitativoFieldValidation()){
+      this.event.emit(1)
+      this.emitCualitativa.emit(this.LecturaCualitativa);
+      this.emitCuantitativa.emit(this.LecturaCuantitativa);
+    }
   }
   back() {
     this.event.emit(-1)
