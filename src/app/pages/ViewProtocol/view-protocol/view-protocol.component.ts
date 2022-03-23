81b31c3d89b9c94a231c3b79cc20cff361779776
msoraire <msoraire@dev.azure.com>
2022-03-23 04:10:31 -0300

    viewProtocolPDFMethods

--- src/app/pages/ViewProtocol/view-protocol/view-protocol.component.ts
+++ src/app/pages/ViewProtocol/view-protocol/view-protocol.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-view-protocol',
@@ -6,10 +12,126 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-protocol.component.css']
 })
 export class ViewProtocolComponent implements OnInit {
+  @ViewChild('Encabezado') Encabezado: ElementRef;
+  @ViewChild('P1CA') P1CA: ElementRef;
+  @ViewChild('P2EO') P2EO: ElementRef;
+  @ViewChild('P3R') P3R: ElementRef;
+  @ViewChild('P4D') P4D: ElementRef;
+  @ViewChild('P5L') P5L: ElementRef;
+  @ViewChild('P6E') P6E: ElementRef;
+  @ViewChild('TablaCuantitativa') TablaCuantitativa: ElementRef;
+  @ViewChild('TablaCualitativa') TablaCualitativa: ElementRef;
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+
+  }
+  createFullPDF() {
+    html2canvas(this.Encabezado.nativeElement).then((canvas) => {
+      const encabezado = canvas.toDataURL();
+      html2canvas(this.P1CA.nativeElement).then((canvas) => {
+        const p1 = canvas.toDataURL();
+        html2canvas(this.P2EO.nativeElement).then((canvas) =>{
+          const p2 = canvas.toDataURL();
+          html2canvas(this.P3R.nativeElement).then((canvas)=>{
+            const p3 = canvas.toDataURL();
+            html2canvas(this.P4D.nativeElement).then((canvas)=>{
+              const p4 = canvas.toDataURL();
+              html2canvas(this.P5L.nativeElement).then((canvas)=>{
+                const p5 = canvas.toDataURL();
+                html2canvas(this.P6E.nativeElement).then((canvas)=>{
+                  const p6 = canvas.toDataURL();
+                  html2canvas(this.TablaCuantitativa.nativeElement).then((canvas)=>{
+                    const Tcuant = canvas.toDataURL();
+                    html2canvas(this.TablaCualitativa.nativeElement).then((canvas)=>{
+                      const Tcual = canvas.toDataURL();
+                      const docDefinition = {
+                        content:[{
+                          image: encabezado,
+                          width:500,
+                          margin: [10 , 10]
+                        },
+                      {
+                        image: p1,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: p2,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: p3,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: p4,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: p5,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: p6,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: Tcuant,
+                        width:500,
+                        margin: [10 , 10]
+                      },
+                      {
+                        image: Tcual,
+                        width:500,
+                        margin: [10 , 10]
+                      }]
+                      }
+                      pdfMake.createPdf(docDefinition).open();
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  }
+  createSummaryPDF(){
+    html2canvas(this.Encabezado.nativeElement).then((canvas) => {
+      const encabezado = canvas.toDataURL()
+      html2canvas(this.TablaCuantitativa.nativeElement).then((canvas) => {
+        const Tcuant = canvas.toDataURL();
+        html2canvas(this.TablaCualitativa.nativeElement).then((canvas) => {
+          const Tcual = canvas.toDataURL();
+          const pdf = {
+            content:[{
+              image: encabezado,
+              width:500,
+              margin: [10 , 10]
+            },{
+              image: Tcuant,
+              width:500,
+              margin: [10 , 10]
+            },{
+              image: Tcual,
+              width:500,
+              margin: [10 , 10]
+            }]
+          }
+          pdfMake.createPdf(pdf).open();
+        })
+      })
+    })
+
   }
 
 }
